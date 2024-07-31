5f81a5682c7fd5d14372c97ad88c5efab7939a7b
haruki7049 <tontonkirikiri@gmail.com>
2024-07-31 14:45:23 +0900

    style: Init treefmt-nix

--- flake.nix
+++ flake.nix
@@ -5,20 +5,26 @@
     treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    treefmt-nix,
-    ...
-  }:
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , treefmt-nix
+    , ...
+    }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
     let
       pkgs = import nixpkgs {
         inherit system;
       };
+      treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
     in
     {
+      formatter = treefmtEval.config.build.wrapper;
+      checks = {
+        formatting = treefmtEval.config.build.check self;
+      };
+
       devShells.default = pkgs.mkShell {
         packages = [
           pkgs.nil
